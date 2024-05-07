7b679ab63dc8f323e03dca83de1b1f1696d460aa
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2024-05-07 13:02:53 -0400

    more changes

---
+++ header.js
@@ -0,0 +1,79 @@
+// Create dropdown menu function
+function createDropdown(title, links) {
+    // Create dropdown container
+    var dropdown = document.createElement("div");
+    dropdown.classList.add("dropdown");
+
+    // Create button
+    var button = document.createElement("button");
+    button.classList.add("dropbtn");
+    button.textContent = title;
+
+    // Append button to dropdown container
+    dropdown.appendChild(button);
+
+    // Create dropdown content container
+    var dropdownContent = document.createElement("div");
+    dropdownContent.classList.add("dropdown-content");
+
+    // Create links and append to dropdown content container
+    links.forEach(function(link) {
+        var a = document.createElement("a");
+        a.href = link.url;
+        a.textContent = link.text;
+        dropdownContent.appendChild(a);
+
+        //Check if it contains an indicator
+        if (link.indicator)
+            {
+                var img = document.createElement("img");
+                img.src = link.indicator;
+                img.classList.add("dropdown-indicator");
+                // Append image next to text
+                a.appendChild(img);
+            }
+    });
+
+    // Append dropdown content container to dropdown container
+    dropdown.appendChild(dropdownContent);
+
+    // Append dropdown container to body
+    document.getElementById("header").appendChild(dropdown);
+}
+
+// Create dropdown menus
+createDropdown("Pages", [
+    {url: "index.html", text: "Home"},
+    {url: "#", text: "About"},
+    {url: "#", text: "Contact"}
+]);
+
+createDropdown("Games", [
+    {url: "bloodfuel.html", text: "Blood Fuel ", indicator: "Media/Indicators/Unity.png"},
+    {url: "#", text: "Dungeon Bounties ", indicator: "Media/Indicators/UE5.png"},
+    {url: "#", text: "Knee Cappers ", indicator: "Media/Indicators/UE5.png"},
+    {url: "#", text: "Thruster ", indicator: "Media/Indicators/Unity.png"},
+    {url: "#", text: "Poultry Panic ", indicator: "Media/Indicators/Clickteam.png"}
+]);
+
+createDropdown("Software", [
+    {url: "#", text: "ATLAS Character Maker"},
+    {url: "#", text: "Garden Discord Bot"},
+    {url: "#", text: "RPG Discord Bot"}
+]);
+
+createDropdown("Programming", [
+    {url: "#", text: "Procedural Generation"}
+]);
+
+createDropdown("Projects", [
+    {url: "#", text: "Infinite Finality"},
+    {url: "#", text: "Sky Jellies"},
+    {url: "#", text: "ATLAS"}
+]);
+
+createDropdown("Art", [
+    {url: "#", text: "Sketches"},
+    {url: "#", text: "Concept Art"},
+    {url: "#", text: "Finished Pieces"}
+]);
